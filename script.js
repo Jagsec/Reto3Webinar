ede00ada31b08c3162b7f83242019337a85b9ff1
Jaime Govea <jaimegovea@protonmail.com>
2021-02-02 22:04:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var message = "Busca el directorio donde está la flag"
+var message2 = "Encontraste la flag!"
+
+function handleClick(){
+    alert(message)
+}
+function handleClick2(){
+    alert(message2)
+}
